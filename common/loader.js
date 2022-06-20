997315325ed6b30ddb302d47d3d32aca8605e597
dejosemario <josemariaodumodu@gmail.com>
2022-06-20 13:31:18 +0100

    add loader modules and create a load animation

--- common/loader.js
+++ common/loader.js
@@ -1,18 +1,16 @@
-import Image from 'next/image';
+import Image from 'next/image'
+import styles from '../styles/Loader.module.scss'
 
-function Loader(props) {
-  const style = {
-    borderRadius: '50%',
-  }
+function Loader() {
   return (
     <div>
       <Image
-        className='loader'
+        className={styles.loader}
         src="/images/loader.jpg"
         alt="loader"
         width='100'
         height='100'
-        style={style}
+      // style={styles.element}
       />
     </div>
   );
