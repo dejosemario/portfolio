4462fd941957666ea34c4d98b49e34f8d7a1909e
dejosemario <josemariaodumodu@gmail.com>
2022-06-20 13:35:14 +0100

    add timeout for the loader

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Loader from '../common/loader'
+import { useState } from 'react'
 
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  setTimeout(() => {
+    setLoading(false)
+  }
+    , 6000)
+
   return (
-    <div className={styles.loader}>
+    <div className={styles.loader} style={loading ? { display: 'flex' } : { display: 'none' }}>
       <Loader />
     </div>
   )
